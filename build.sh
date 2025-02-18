73585a9a74f8ea94b300fd1405b046d2f40d6eff
koko-07870 <ko282ko82@gmail.com>
2025-02-18 13:59:21 +0530

    build.sh: Switch to clang 20 (r547379)
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ export KBUILD_BUILD_USER="koko"
 # Clang
 clangbin=../clang/bin/clang
 if ! [ -a $clangbin ]; then
-wget -O toolchain.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r530567.tar.gz
+wget -O toolchain.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz
 mkdir -p ${PWD}/../clang
 tar -xzf toolchain.tar.gz -C ${PWD}/../clang
 rm -rf toolchain.tar.gz
