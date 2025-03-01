cae0180ea071434206432cb11d8c11e5f6411bee
koko-07870 <ko282ko82@gmail.com>
2025-03-01 20:16:46 +0530

    ARM64: configs: merge debugfs.config
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ tar -xzf toolchain.tar.gz -C ${PWD}/../clang
 rm -rf toolchain.tar.gz
 fi
 
-make O=out ARCH=arm64 vendor/pixelos-${DEVICE}_defconfig vendor/debugfs.config
+make O=out ARCH=arm64 vendor/pixelos-${DEVICE}_defconfig
 PATH="${PWD}/../clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
